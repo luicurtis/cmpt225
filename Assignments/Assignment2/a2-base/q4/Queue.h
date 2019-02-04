ce94b7a49bfd53dd7e5a0923ec139a4fd74ff11d
Curtis Lui <lui_curtis@hotmail.com>
2019-02-03 18:01:49 -0800

    Add files via upload
    Finished Assignment 2, ready for linux testing

--- Assignments/Assignment2/a2-base/q4/Queue.h
+++ Assignments/Assignment2/a2-base/q4/Queue.h
@@ -4,14 +4,15 @@
  * Description: Implementation of an int sequence with enqueue/dequeue ...
  * Class Invariant: ... in FIFO order
  *
- * Author:
- * Date:
+ * Author:  Curtis Lui
+ * Date:    February 3 2019
  */
   
-class Queue {
+class Queue
+{
     private:
         static unsigned const INITIAL_SIZE = 6;
-        int elements[INITIAL_SIZE];  // replace this by int * elements in Question 4(a)
+        int * elements;  // replace this by int * elements in Question 4(a)
 
         unsigned elementCount;  // number of elements in the queue
         unsigned capacity;      // number of cells in the array
@@ -22,20 +23,19 @@ class Queue {
         // Desc:  Constructor
         Queue();
 
+        // Desc: Destructor
+        ~Queue();
 
         // Desc:  Inserts element x at the back (O(1))
         void enqueue(int x);
-
 
         // Desc:  Removes the frontmost element (O(1))
         //  Pre:  Queue not empty
         void dequeue();
 
-
         // Desc:  Returns a copy of the frontmost element (O(1))
         //  Pre:  Queue not empty
         int peek() const;
-
 
         // Desc:  Returns true if and only if queue empty (O(1))
         bool isEmpty() const;
