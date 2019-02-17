25c6659e6481a3314b79dd23095a8a812b713d54
luicutis <lui_curtis@hotmail.com>
2019-02-17 08:35:51 -0800

    Incomplete implementation of the copy constructor

--- Assignments/Assignment3/PriorityQueue.cpp
+++ Assignments/Assignment3/PriorityQueue.cpp
@@ -11,6 +11,35 @@
 
 #include "PriorityQueue.h"  // Header file
 
+// Default Constructor
+template <class T>
+PriorityQueue<T>::PriorityQueue() : head(NULL), elementCount(0)
+{
+    
+} // End of PriorityQueue()
+
+// Copy Constructor
+template <class T>
+PriorityQueue<T>::PriorityQueue(const PriorityQueue &rhsPriorityQueue)
+{
+    Node<T> *curRHS = rhsPriorityQueue.head;
+    Node<T> *curThis = NULL;
+    Node<T> newNode = new Node<T>;
+    head = newNode;
+    head->data = curRHS->data;
+    curThis = head;
+
+    while (curRHS != NULL)
+    {
+        Node<T> newNode = new Node<T>;  // Create new node to add to pQueue
+        curThis->next = newNode;        // Link prev Node to newNode
+        curThis = newNode;              // Point to the newNode
+        curThis->data = curRHS->data;   // Set Node data equal to RHS
+
+        curRHS = curRHS->next;          // Move to next Node in RHS
+    }
+}
+
 
 
 //  End of implementation file.
