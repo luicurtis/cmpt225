c1e38e830e30caab64013e3fa863e7825d19112c
luicutis <lui_curtis@hotmail.com>
2019-03-03 12:17:06 -0800

    Finished outlining the algorithm. Need to fix bugs

--- Labs/Lab6/lab6-test/merge_sort.cpp
+++ Labs/Lab6/lab6-test/merge_sort.cpp
@@ -4,22 +4,23 @@ using namespace std;
 void mergeSort(int arr[], int low, int high);
 void merge(int arr[], int low, int mid, int mid1, int high);
 
-
-
-
 // Perform mergeSort to sort an array of integers.
 // Inputs:
 //   arr is the input array of arrSize integers.
 //   low and high are the indices to sort to/from, e.g. 0 arrSize-1 to sort entire array
 // Postcondition:
 //   arr[low] ... arr[high] will contain the original values, sorted in increasing order.
-void mergeSort(int arr[], int low, int high) {
-
-
-	// TO DO:: Fill in this code.
-
+void mergeSort(int arr[], int low, int high)
+{
+	// Base case
+	if (low < high)
+	{
+		int mid = (low + high) / 2;			 // Find midpoint of array
+		mergeSort(arr, low, mid);			 // Recursively partition left side of array until each subarray is length 1
+		mergeSort(arr, (mid + 1), high);		 // Recursively partition right side of array until each subarray is length 1
+		merge(arr, low, mid, (mid + 1), high); // Recursively merge left and right partitions
+	}
 }
-
 
 // Perform a merge of two sorted sub-arrays.
 // Inputs:
@@ -31,21 +32,61 @@ void mergeSort(int arr[], int low, int high) {
 //   These should be disjoint, contiguous sub-arrays too.
 // Postcondition:
 //   arr[low]...arr[high] is sorted in increasing.
-void merge(int arr[], int low, int mid, int mid1, int high) {
+void merge(int arr[], int low, int mid, int mid1, int high)
+{
+	// Initialize Indices 
+	int arrIndex1 = low;
+	int arrIndex2 = mid1;
 
+	// Create temp array
+	int arrSize = (high - low) + 1;
+	int tempArr[arrSize];
+	int tempArrIndex = low;
 
+	// While both subarrays contain unmerged elements
+	while (arrIndex1 < mid && arrIndex2 < high)
+	{
+		if (arr[arrIndex1] < arr[arrIndex2])
+		{
+			tempArr[tempArrIndex] = arr[arrIndex1];			
+			arrIndex1++;
+		}
+		else
+		{
+			tempArr[tempArrIndex] = arr[arrIndex2];
+			arrIndex2++;
+		}
 
-	// TO DO:: Fill in this code.
+		tempArrIndex++;
+	}
+	// If subArray1 has unmerged items
+	while (arrIndex1 < mid)
+	{
+		tempArr[tempArrIndex] = arr[arrIndex1];			
+		arrIndex1++;
+		tempArrIndex++;
+	}
 
+	// If subArray2 has unmerged items
+	while (arrIndex2 < high)
+	{
+		tempArr[tempArrIndex] = arr[arrIndex2];
+		arrIndex2++;
+		tempArrIndex++;
+	}
 
+	for (int i = low; i <= high; i++)
+	{
+		arr[i] = tempArr[i];
+	}
 }
-
 
 // A driver program to test mergeSort.
 // Reads in a number of integers (number, then sequence of values).
 // Sorts using mergeSort.
 // Prints the values in sorted order.
-int main (void) {
+int main(void)
+{
 	int n;
 
 	// Get the number of values.
@@ -56,23 +97,24 @@ int main (void) {
 	int *values;
 	values = new int[n];
 
-	for (int i=0; i<n; i++) {
+	for (int i = 0; i < n; i++)
+	{
 		// cout << "Enter a number:" << endl;
 		cin >> values[i];
 	}
 
 	// Call mergeSort to sort entire array.
-	mergeSort(values,0,n-1);
+	mergeSort(values, 0, n - 1);
 
 	// Print values, should be sorted.
-	for (int i=0; i<n; i++) {
+	for (int i = 0; i < n; i++)
+	{
 		cout << values[i] << " ";
 	}
 	cout << endl;
 
-
 	// Free up memory.  Note use of delete [], since this is an array.
-	delete [] values;
+	delete[] values;
 
 	return 0;
 }
